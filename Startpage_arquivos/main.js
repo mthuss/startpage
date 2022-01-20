f7b09e8658adc7d59268b9f21ca3f0bb012bf40b
Moriel <86256436+mthuss@users.noreply.github.com>
2022-01-20 14:18:52 -0300

    Reformatted clock
    removed year, added day of the week and a little time-dependant greeting

--- Startpage_arquivos/main.js
+++ Startpage_arquivos/main.js
@@ -25,17 +25,31 @@ function getDateTime() {
     let hour = dateTime.getHours();
     let minutes = dateTime.getMinutes();
     let seconds = dateTime.getSeconds();
-
+    let dotw = dateTime.getDay();
+        switch(dotw){
+            case 0: dotw='日'; break;
+            case 1: dotw='月'; break;
+            case 2: dotw='火'; break;
+            case 3: dotw='水'; break;
+            case 4: dotw='木'; break;
+            case 5: dotw='金'; break;
+            case 6: dotw='土'; break;
+        }
     if (hour < 0) {
         hour = 24 + hour;
     }
-
+    //Greetings
+    let greeting = 'おい！！';
+    if (hour>12 && hour<19)
+        greeting = 'こんにちは！';
+    else if ((hour>19 && hour<=24) || (hour>0 && hour<6))
+        greeting = 'こんばんは！';
+    else greeting = 'おはよう！';
 //    let date = (day < 10 ? '0' + day : day) + '/' + (month < 10 ? '0' + month : month) + '/' + dateTime.getFullYear();
-
-    let date = dateTime.getFullYear() + '年' + (month < 10 ? '0' + month : month) + '月' + (day < 10 ? '0' + day : day) + '日';
+    let date = (month < 10 ? '0' + month : month) + '月' + (day < 10 ? '0' + day : day) + '日' + '(' + dotw + ')';
     let time = (hour < 10 ? '0' + hour : hour) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
 
-    return date + '\n' + time;
+    return greeting + '\n' + date + '\n' + time;
 }
 
 function setClock() {
